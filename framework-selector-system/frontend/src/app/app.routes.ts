c31042ff33d0c09bc72e85165e538462729f45d4
Peheliya Dhanuka Navod <140178910+peheliyanavod@users.noreply.github.com>
2026-03-09 00:52:21 +0530

    Create Learn page and UI for enter requirements

--- framework-selector-system/frontend/src/app/app.routes.ts
+++ framework-selector-system/frontend/src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home';
+import { LearnComponent } from './components/learn/learn';
+import { EvaluatorComponent } from './components/evaluator/evaluator';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'learn', component: LearnComponent },
+  { path: 'evaluate', component: EvaluatorComponent },
   { path: '**', redirectTo: '' }
 ];
---
+++ framework-selector-system/frontend/src/app/components/learn/learn.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-learn',
+  imports: [],
+  templateUrl: './learn.html',
+  styleUrl: './learn.css',
+})
+export class LearnComponent {}
